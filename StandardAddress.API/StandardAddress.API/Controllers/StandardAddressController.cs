07d2334388d39a8b562a1672fef5659710e18a1c
VovaGul <vovagul1999@mail.ru>
2023-11-30 21:37:17 +0500

    add StandardAddressController

---
+++ StandardAddress.API/StandardAddress.API/Controllers/StandardAddressController.cs
@@ -0,0 +1,25 @@
+﻿using Dadata;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StandardAddress.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StandardAddressController : ControllerBase
+    {
+
+        [HttpGet(Name = "StandardizeAddress")]
+        public async Task<IEnumerable<WeatherForecast>> StandardizeAddressAsync()
+        {
+            var token = "Replace with Dadata API key";
+            var secret = "Replace with Dadata secret key";
+
+            var api = new CleanClientAsync(token, secret);
+            // or any of the following, depending on the API method
+            var api1 = new SuggestClientAsync(token);
+            var api2 = new OutwardClientAsync(token);
+            var api3 = new ProfileClientAsync(token, secret);
+        }
+    }
+}
