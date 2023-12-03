10bd20e8909bbe904a1ce1c023234045c646d9e9
VovaGul <vovagul1999@mail.ru>
2023-12-03 21:39:50 +0500

    реализовал связь клиента и сервера

--- StandardAddress.API/StandardAddress.API/Program.cs
+++ StandardAddress.API/StandardAddress.API/Program.cs
@@ -1,8 +1,10 @@
+using Microsoft.Extensions.DependencyInjection;
+
 namespace StandardAddress.API
 {
     public class Program
     {
-        readonly static string CORSOpenPolicy = "OpenCORSPolicy";
+        //readonly static string CORSOpenPolicy = "OpenCORSPolicy";
 
         public static void Main(string[] args)
         {
@@ -15,10 +17,10 @@ namespace StandardAddress.API
             builder.Services.AddAutoMapper(typeof(AddressProfile));
             builder.Services.AddCors(options =>
             {
-                options.AddPolicy(
-                    name: CORSOpenPolicy,
-                    builder => {
-                        builder.WithOrigins("*").AllowAnyHeader().AllowAnyMethod();
+                options.AddPolicy("AllowAllOrigins",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
                     });
             });
 
@@ -35,15 +37,47 @@ namespace StandardAddress.API
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseAccessControlAllowOriginAlways();
             app.UseHttpsRedirection();
-            app.UseCors(CORSOpenPolicy);
+            app.UseCors("AllowAllOrigins");
             app.UseAuthorization();
 
 
             app.MapControllers();
 
             app.Run();
+        }
+    }
+
+    public static class AccessControlAllowOriginAlwaysExtensions
+    {
+        public static IApplicationBuilder UseAccessControlAllowOriginAlways(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<AccessControlAllowOriginAlways>();
+        }
+    }
+
+    public class AccessControlAllowOriginAlways
+    {
+        private readonly RequestDelegate _next;
+        private const string AccessControlAllowOrigin = "Access-Control-Allow-Origin";
+        public AccessControlAllowOriginAlways(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public Task InvokeAsync(HttpContext context)
+        {
+            context.Response.OnStarting(() =>
+            {
+                if (!context.Response.Headers.ContainsKey(AccessControlAllowOrigin))
+                {
+                    context.Response.Headers.Add(AccessControlAllowOrigin, "http://localhost:4200");
+                }
+                return Task.CompletedTask;
+            });
+            return _next(context);
         }
     }
 }
