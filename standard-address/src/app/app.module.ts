e8fa1744844fe648388603062603751c80bbeb91
VovaGul <vovagul1999@mail.ru>
2023-12-07 19:56:18 +0500

    добавил страницу помощи

--- standard-address/src/app/app.module.ts
+++ standard-address/src/app/app.module.ts
@@ -8,18 +8,21 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HelpPageComponent } from './help-page/help-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    HelpPageComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       {path: 'home', component: HomeComponent},
       {path: '', component: HomeComponent},
+      {path: 'help-page', component: HelpPageComponent},
       {path: '**', component: PageNotFoundComponent}
     ]),
     BrowserAnimationsModule,
