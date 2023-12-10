f6f707e16ca1ecaa75297d9d257948c89376d11f
VovaGul <vovagul1999@mail.ru>
2023-12-10 14:53:13 +0500

    переименовал

--- StandardAddress.API/StandardAddress.API/DadataService.cs
+++ StandardAddress.API/StandardAddress.API/DadataService.cs
@@ -9,24 +9,24 @@ namespace StandardAddress.API
     public class DadataService
     {
         private readonly HttpClient _httpClient;
-        private readonly DadataAuth _auth;
+        private readonly DadataAuth _dadataAuth;
 
         public DadataService(HttpClient httpClient, IOptions<DadataAuth> options) 
         {
             _httpClient = httpClient;
 
-            _auth = options.Value;
+            _dadataAuth = options.Value;
         }
 
         public async Task<Address> CleanAsync(string rawAddress)
         {
             string jsonPayload = $"[ \"{rawAddress}\" ]";
 
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, _auth.ApiUrl);
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, _dadataAuth.ApiUrl);
             // Добавляем заголовки
             request.Headers.Add("Accept", "application/json");
-            request.Headers.Add("Authorization", $"Token {_auth.Token}");
-            request.Headers.Add("X-Secret", _auth.Secret);
+            request.Headers.Add("Authorization", $"Token {_dadataAuth.Token}");
+            request.Headers.Add("X-Secret", _dadataAuth.Secret);
 
             // Устанавливаем полезную нагрузку
             request.Content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
